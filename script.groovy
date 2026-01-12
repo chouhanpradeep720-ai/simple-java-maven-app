edb6f86800b5d87c147968e51e2f598432d28d96
chouhanpradeep720-ai <chouhanpradeep720@gmail.com>
2026-01-12 17:37:45 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,7 +3,7 @@ def buildjar() {
    sh 'mvn package'
 }
 def buildpush() {
-  withCredentials([usernamePassword(credentialsId: 'nexus-id', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+  withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
                         sh 'docker build -t pradeepchouhan115/docker.repo:jma-2.4 .'
                         sh 'echo $PASS | docker login -u $USER --password-stdin'
                         sh 'docker push pradeepchouhan115/docker.repo:jma-2.4'
