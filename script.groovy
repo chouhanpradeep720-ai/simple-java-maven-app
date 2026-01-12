b13898c4ab051774fab238e858ec5dd611efd8ce
chouhanpradeep720-ai <chouhanpradeep720@gmail.com>
2026-01-12 15:42:47 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,16 @@
+def buildjar() {
+   echo "building a jar..."
+   sh 'mvn package'
+}
+def buildpush() {
+  withCredentials([usernamePassword(credentialsId: 'nexus-id', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                        sh 'docker build -t 172.17.0.1:8083/jma:2.3 .'
+                        sh 'echo $PASS | docker login -u $USER --password-stdin 172.17.0.1:8083'
+                        sh 'docker push 172.17.0.1:8083/jma:2.3'
+                    }
+}
+def deploy() {
+  echo "deploying this project...."
+}
+return this
 
