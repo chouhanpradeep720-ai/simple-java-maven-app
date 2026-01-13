297e601e2fe40c4e7ac975536a0e3830fbcd6009
chouhanpradeep720-ai <chouhanpradeep720@gmail.com>
2026-01-13 17:10:37 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,5 @@
-def buildjar() {
-   echo "building a jar..."
-   sh 'mvn package'
-}
-def buildpush() {
-  withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-                        sh 'docker build -t pradeepchouhan115/docker.repo:jma-2.4 .'
-                        sh 'echo $PASS | docker login -u $USER --password-stdin'
-                        sh 'docker push pradeepchouhan115/docker.repo:jma-2.4'
-                    }
-}
-def deploy() {
+
+def deployapp() {
   echo "deploying this project...."
 }
 return this
